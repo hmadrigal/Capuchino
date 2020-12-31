f9ebc786b5e0326bf51e9f85acbbd1dae9daee5a
hmadrigal <hfmad@hotmail.com>
2020-12-30 19:38:51 -0600

    WIP

---
+++ src/Capuchino.AvaloniaApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capuchino.AvaloniaApp.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
